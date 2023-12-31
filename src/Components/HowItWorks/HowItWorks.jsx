77fe6a9058be89bdfedbc55b049645d10b65d465
alotav <alonsotaverna@hotmail.com>
2023-12-31 15:55:47 -0300

    transition + css

--- src/Components/HowItWorks/HowItWorks.jsx
+++ src/Components/HowItWorks/HowItWorks.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import "./HowItWorks.css";
 import Message1 from "./Message1";
 import Message2 from "./Message2";
@@ -6,24 +7,32 @@ import { useEffect, useState } from "react";
 
 const HowItWorks = () => {
   const [showTitle, setShowTitle] = useState(false);
-  const [showMessages, setShowMessages] = useState(false);
+  const [showMessage1, setShowMessage1] = useState(false);
+  const [showMessage2, setShowMessage2] = useState(false);
+  const [showMessage3, setShowMessage3] = useState(false);
 
   useEffect(() => {
-    const messageTimer = setTimeout(() => {
-      setShowMessages(true);
+    const message1Timer = setTimeout(() => {
+      setShowMessage1(true);
     }, 200);
+    const message2Timer = setTimeout(() => {
+      setShowMessage2(true);
+    }, 300);
+    const message3Timer = setTimeout(() => {
+      setShowMessage3(true);
+    }, 400);
     const titleTimer = setTimeout(() => {
       setShowTitle(true);
     }, 100);
-    return () => clearTimeout(messageTimer, titleTimer);
+    return () => clearTimeout(message1Timer, titleTimer);
   }, []);
 
   return (
     <div className="howItWorksContainer">
       <h1 className={` ${showTitle ? "show" : "message"} `}>¿Cómo Funciona?</h1>
-      <Message1 className={` ${showMessages ? "show" : "message"} `} />
-      <Message2 className={` ${showMessages ? "show" : "message"} `} />
-      <Message3 className={` ${showMessages ? "show" : "message"} `} />
+      <Message1 className={` ${showMessage1 ? "show" : "message"} `} />
+      <Message2 className={` ${showMessage2 ? "show" : "message"} `} />
+      <Message3 className={` ${showMessage3 ? "show" : "message"} `} />
     </div>
   );
 };
