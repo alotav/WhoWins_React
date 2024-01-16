af73120508d9be513feed82ea907f43c3df52a6c
alotav <alonsotaverna@hotmail.com>
2024-01-15 22:09:46 -0300

    last details

--- src/Components/Lottery/Players.jsx
+++ src/Components/Lottery/Players.jsx
@@ -34,7 +34,7 @@ const Players = ({ className }) => {
   return (
     <div className={`playersContainer ${className}`}>
       <h3>Instrucciones</h3>
-      <p>Ingres los nombres de los participantes separados por coma.</p>
+      <p>Ingrese los nombres de los participantes separados por coma.</p>
       <p>Haga click en &quot;Sortear!&quot; y espere el resultado.</p>
       <textarea
         name="players"
